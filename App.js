794ed26716072fcce99227a1f1e141e41d1a25b7
Franco Capiglioni <fcapiglioni@uoc.edu>
2023-09-11 11:10:42 +0200

    fdsfd

---
+++ App.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+const { stringify } = require('querystring');
+require('dotenv').config({ path: 'variables.env' });//connectar con archivo variables.env
+//conectem servidor
+mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true});//aqui estaba el link que no queremos que se vea en github y por eso emos creado la carpeta variables.env para darle el valor de todo el link a "DB"
+const db = mongoose.connection;
+
+
+db.on('error', (error) => console.error(error));// equivale al .catch 
+db.once('open', () => console.log('Conexion establecida !!!'));// .once equivale al .then porque esta funcion ya viene con el mongoose
+
+const objetoEsquema=new mongoose.Schema({ //diem que les dades que guardem son tipo text
+    cancion: String,
+    artista: String,
+    genero: String
+});
+
+const Objeto  =mongoose.model('Objeto', objetoEsquema);// el primer Objeto no es el mateix que 'Objeto'
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use('/public', express.static(path.join(__dirname,'public')))//
+//routing 
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname,"public", "Index.html"));
+})
+
+app.post("/recomendacion", async (req, res) => {
+    let usuarioObj = req.body;
+    console.log(usuarioObj);
+
+    try {
+        let nuevoObjeto = new Objeto(usuarioObj);
+        await nuevoObjeto.save();
+
+        let mismogenero = await Objeto.find({ genero: usuarioObj.genero });
+        let objetoAEnviar;
+
+        if (mismogenero && mismogenero.length > 0) {
+            let mismoartista = mismogenero.filter(obj => obj.artista === usuarioObj.artista);
+
+            if (mismoartista && mismoartista.length > 0) {
+                let difcancion = mismoartista.filter(obj => obj.cancion !== usuarioObj.cancion);
+
+                if (difcancion && difcancion.length > 0) {
+                    objetoAEnviar = difcancion[Math.floor(Math.random() * difcancion.length)];
+                } else {
+                    objetoAEnviar = mismoartista[Math.floor(Math.random() * mismoartista.length)];
+                }
+            } else {
+                objetoAEnviar = mismogenero[Math.floor(Math.random() * mismogenero.length)];
+            }
+        } else {
+            let todos = await Objeto.find();
+            objetoAEnviar = todos[Math.floor(Math.random() * todos.length)];
+        }
+
+        if (objetoAEnviar) {
+            res.json(objetoAEnviar);
+        } else {
+            res.status(500).send('No se pudo encontrar un objeto para enviar');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al procesar la solicitud');
+    }
+
+
+});
+ const PORT = process.env.PORT || 3000;//usa una variable que se llame PORT sino existe usara la 3000
+app.listen(PORT, () =>{
+    console.log ("Servidor corriendo");
+});
+//cambio
---
+++ public/Recomendador.js
@@ -0,0 +1,247 @@
+let songsearch = document.getElementById("song-btn");
+songsearch.addEventListener("click", SearchSong);
+
+let artistsearch = document.getElementById("artist-btn");
+artistsearch.addEventListener("click", SearchArtist);
+
+var generos = [
+"Bachata",
+"Blues",
+"Country",
+"Cumbia",
+"Dance",
+"Disco",
+"Downtempo",
+"Drum and Bass",
+"Electrónica",
+"Flamenco",
+"Folk",
+"Funk",
+"Funky",
+"Fusión",
+"Gospel",
+"Grunge",
+"Hard Rock",
+"Heavy Metal",
+"Hip-hop",
+"Hip House",
+"House",
+"Indie",
+"Indie Pop",
+"Indie Rock",
+"Industrial",
+"Instrumental",
+"J-Pop",
+"Jazz",
+"K-Pop",
+"Lounge",
+"Merengue",
+"Metal",
+"Música clásica contemporánea",
+"Música experimental",
+"Música industrial",
+"Música minimalista",
+"Música vocal",
+"New Age",
+"New Wave",
+"Opera",
+"Pop",
+"Post-punk",
+"Punk",
+"R&B",
+"Rap",
+"Reggae",
+"Reggaetón",
+"Rock",
+"Rock alternativo",
+"Rock progresivo",
+"Rock psicodélico",
+"Rock sinfónico",
+"Rhythm and Blues (R&B)",
+"Salsa",
+"Samba",
+"Ska",
+"Ska punk",
+"Soul",
+"Swing",
+"Synth-pop",
+"Tango",
+"Techno",
+"Trance",
+"Trap",
+"Trópical",
+"Vallenato",
+"World Music"
+  ];
+
+  
+let obj = {
+    cancion:"", artista:"",
+    genero:""
+}
+
+
+  const select_genre = document.getElementById("select_generos")
+  for (let index = 0; index < generos.length; index++) {
+    let genre = generos[index]
+    let option = document.createElement("option");
+    option.value = genre;
+    option.innerHTML= genre;
+    select_genre.appendChild(option);
+  }
+function SearchSong() {
+    let inp = document.getElementById("search-song");
+    if (inp.value.length < 3) {
+        return
+    }
+    //ella&baila&sola
+    let value = inp.value;
+    const nombreCancionEncoded = encodeURIComponent(value);
+    const url = `https://itunes.apple.com/search?term=${nombreCancionEncoded}&media=music`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+
+            var box = document.querySelector("#song-suggestion-container");
+            box.innerHTML = "";
+            var songs = data.results.slice(0, 7);
+
+            for (let index = 0; index < songs.length; index++) {
+                var el = document.createElement("div");
+                el.classList.add("song");
+                el.innerHTML = `<span class='song-title'>${songs[index].trackName}</span><span>by ${songs[index].artistName}</span>`
+                box.append(el);
+                el.addEventListener("click", () => {
+                    let fav = document.querySelector("#favorite-song");
+                    fav.innerHTML = "";
+                    let chosen = document.createElement("div");
+                    chosen.classList.add("chosen-song");
+                    chosen.innerHTML = `<h3>${songs[index].trackName}</h3><span>by ${songs[index].artistName}</span><br><img class='img-song' src="${songs[index].artworkUrl100}"<br>`
+                    obj.cancion=songs[index].trackName;// aqui ponemos la cancion que emos clicado dentro de el apartado objeto , debtro de el elemento cancion.
+                    let audio = document.createElement("audio");
+                    audio.controls = true;
+                    audio.src = songs[index].previewUrl;
+                    chosen.appendChild(audio);
+                    fav.append(chosen);
+                });
+            }
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+
+
+function SearchArtist() {
+    let inp = document.getElementById("search-artist");
+    if (inp.value.length < 3) {
+        return
+    }
+    let value = inp.value;
+    const nombreArtistaEncoded = encodeURIComponent(value);
+    const url = `https://itunes.apple.com/search?term=${nombreArtistaEncoded}&entity=musicArtist`;
+    fetch(url).then(response => response.json())
+        .then((data) => {
+            let fav = document.querySelector("#artist-suggestion-container");
+            fav.innerHTML = "";
+
+            let artists = data.results.slice(0, 5);
+
+            for (let posicion = 0; posicion < artists.length; posicion++) {
+                let el = document.createElement("div");
+                el.classList.add("artist");
+                el.innerHTML = `<span>${artists[posicion].artistName}</span>`;
+                el.addEventListener("click", ()=>{//aqui fem que cuan seleccioninin un artista pasi una funcio de flecha
+                    let informacionartista = document.getElementById("favorite-artist");
+                    informacionartista.innerHTML = ""
+                    let chosen = document.createElement("div");
+                    chosen.classList.add("chosen-artist")
+                    chosen.innerHTML=`<h3 class="favorite-artist-name">${artists[posicion].artistName}</h3><span class='favorite-artist-genre'>Genre: ${artists[posicion].primaryGenreName}</span><a target='_blank' class='favorite-artist-link' href='${artists[posicion].artistLinkUrl}'>Ver artista</a>`;
+                    obj.artista=artists[posicion].artistName// es lo mismo que lo otro pero con el artista.
+                    informacionartista.append(chosen);
+
+
+                });
+                fav.append(el);
+
+            }
+        }).catch(error => console.log(error));
+        
+
+}
+
+function reset()  {
+    var box = document.querySelector("#song-suggestion-container");
+            box.innerHTML = "";
+            let fav = document.querySelector("#favorite-song");
+            fav.innerHTML = "";
+            let fav2 = document.querySelector("#artist-suggestion-container");
+            fav2.innerHTML = "";
+            let informacionartista = document.getElementById("favorite-artist");
+                    informacionartista.innerHTML = "";
+}
+
+
+
+var send = document.getElementById("send-btn")
+
+ send.addEventListener("click", ()=> {
+    if (obj.cancion != "" && obj.artista != ""){
+        obj.genero = select_genre.value;
+        document.querySelector("#loading").classList.remove('loading-hidden');
+        document.querySelector("#loading").classList.add('loading-visible');
+        reset();
+        console.log(obj);
+        fetch("/recomendacion", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(obj),// convertimos los tres elementos de obj en jason para que el servidor lo pueda leer, los elementos son las repuestas que nos da el usuario
+        }).then(response => response.json()).then(data=>{
+            console.log(data);//muestra el resultado por consola, data es la respuesta
+            document.querySelector("#loading").classList.add('loading-hidden');//quitamos animacion carga
+            document.querySelector("#loading").classList.remove('loading-visible');
+            mostrarrecomendacion(data);
+        }).catch(error=> console.log(error));
+        
+    }
+ 
+ });
+
+
+
+ function mostrarrecomendacion(value){
+    const nombreCancionEncoded = encodeURIComponent(value.cancion);//solo cojemos la cancion del resultado, i lo metemos en NombreCancionEncoded
+    const url = `https://itunes.apple.com/search?term=${nombreCancionEncoded}&media=music`;
+
+    fetch(url)//es una promesa, hace conexion con servidor i se queda esperando
+        .then(response => response.json())//lo pasa a json, formato leible
+        .then(data => {
+
+            var box = document.querySelector("#recomendation");//coje el elemento div i lo mete en box
+            box.innerHTML = "";
+            var song = data.results[0]//porque queremos el primer resultado
+
+                    let chosen = document.createElement("div");//creamos elemento dic
+                    chosen.classList.add("chosen-song");//le ponemos una classe a div
+                    chosen.innerHTML = `<h3>${song.trackName}</h3><span>by ${song.artistName}</span><br><img class='img-song' src="${song.artworkUrl100}"<br>`
+                    let audio = document.createElement("audio");
+                    audio.controls = true;
+                    audio.src = song.previewUrl;
+                    chosen.appendChild(audio);
+                    box.append(chosen);
+        
+                })
+                
+           
+
+        
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
+ 
